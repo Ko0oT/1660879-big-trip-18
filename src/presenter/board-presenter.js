4cf4e1da3befc9c2825f97ccba6b462edbd26639
Ko0oT <serkokarjalo@gmail.com>
2022-08-23 18:16:29 +0300

    Реализовал отрисовку формы с данными конкретной точки

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,8 +1,7 @@
-// import AddNewPointView from '../view/add-new-point-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import PointView from '../view/point-view.js';
 import BoardView from '../view/board-view.js';
-import { render, RenderPosition } from '../render.js';
+import { render } from '../render.js';
 
 export default class BoardPresenter {
   #boardContainer;
@@ -13,11 +12,10 @@ export default class BoardPresenter {
   #boardPoints;
   #boardDestinations;
   #boardOffers;
-  #chosenDestination;
-  #boardPoint;
+  #chosenOffers;
   #avaliableOffers;
   #boardDestination;
-  #chosenOffers;
+
 
   init = (boardContainer, pointModel) => {
     this.#boardContainer = boardContainer;
@@ -27,31 +25,24 @@ export default class BoardPresenter {
     this.#boardDestinations = [...this.#pointModel.destinations];
     this.#boardOffers = [...this.#pointModel.offers];
 
-    this.#chosenDestination = this.#boardDestinations[0];
-
 
     render(this.#boardComponent, this.#boardContainer);
-
-
-    this.#boardPoint = this.#boardPoints.find((it) => it.id === this.#chosenDestination.id);
-    this.#avaliableOffers = this.#boardOffers.find((it) => it.type === this.#boardPoint.type).offers;
-
-    // render(new EditPointView(this.#chosenDestination, this.#boardDestinations, this.#boardPoint, this.#avaliableOffers), this.#boardComponent.element, RenderPosition.AFTERBEGIN);
 
 
     for (let i = 0; i < this.#boardPoints.length; i++) {
 
       this.#boardDestination = this.#boardDestinations.find((it) => it.id === this.#boardPoints[i].destination);
       this.#chosenOffers = this.#boardOffers.find((it) => it.type === this.#boardPoints[i].type).offers;
-      // render(new PointView(this.#boardPoints[i], this.#boardDestination, this.#chosenOffers), this.#boardComponent.element);
-      this.#renderPoint(this.#boardPoints[i], this.#boardDestination, this.#chosenOffers);
+      this.#avaliableOffers = this.#boardOffers.find((it) => it.type === this.#boardPoints[i].type).offers;
+
+      this.#renderPoint(this.#boardPoints[i], this.#boardDestination, this.#chosenOffers, this.#boardDestinations, this.#avaliableOffers);
     }
   };
 
-  #renderPoint = (points, destination, chosenOffers, chosenDestination, destinations, point, avaliableOffers) => {
-    const pointComponent = new PointView(points, destination, chosenOffers);
-    // const editPointComponent = new EditPointView(chosenDestination, destinations, point, avaliableOffers);
-    const editPointComponent = new EditPointView(this.#chosenDestination, this.#boardDestinations, this.#boardPoint, this.#avaliableOffers);
+
+  #renderPoint = (point, destination, chosenOffers, destinations, avaliableOffers) => {
+    const pointComponent = new PointView(point, destination, chosenOffers);
+    const editPointComponent = new EditPointView(destination, destinations, point, avaliableOffers);
 
 
     const replacePointToForm = () => {
